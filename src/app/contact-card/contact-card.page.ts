c6e065cf6ea00914ee54e06643a9c9b3a9abad20
djd6797 <77515007+djd6797@users.noreply.github.com>
2021-02-21 23:34:47 -0800

    Added Facebook profile info to Contact-Card

--- src/app/contact-card/contact-card.page.ts
+++ src/app/contact-card/contact-card.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { FacebookProviderService } from '../facebook-provider.service';
 
 @Component({
   selector: 'app-contact-card',
@@ -10,20 +11,22 @@ import { Router } from '@angular/router';
     './styles/contact-card.shell.scss'
   ]
 })
-export class ContactCardPage { 
-
-  constructor(
-    public router: Router
-
-  ){}
+export class ContactCardPage implements AfterViewInit {
+  user = null;
+  profilePicSrc: String;
   
+  constructor(
+    public router: Router,
+    private facebookProvider: FacebookProviderService
+  ) { }
+
   // enable push notifications 
-  pushNotificationsEnabled(ionicButton){
-    if(ionicButton.color === 'dark'){
+  pushNotificationsEnabled(ionicButton) {
+    if (ionicButton.color === 'dark') {
       ionicButton.color = 'danger';
       console.log('turing on');
     }
-    else{
+    else {
       ionicButton.color = 'dark';
       console.log('turing off');
     }
@@ -32,6 +35,30 @@ export class ContactCardPage {
   returnHome(): void {
     console.log('go back to landing page');
     this.router.navigate(['/app/categories']);
+  }
+
+  // Disable side menu for this page
+  ionViewDidEnter(): void {
+    //this.menu.enable(false);
+  }
+
+  // Restore to default when leaving this page
+  ionViewDidLeave(): void {
+    //this.menu.enable(true);
+  }
+
+  ngAfterViewInit(): void {
+    this.user = this.facebookProvider.getUser();
+    let userName = document.getElementById("ProfileName");
+    //this.profilePicSrc = <HTMLImageElement>document.getElementById("ProfilePic");
+    if (this.user == null) {
+      userName.innerHTML = "No User";
+      this.profilePicSrc = "./assets/sample-images/notifications/karl.jpg";
+    }
+    else {
+      userName.innerHTML = `${this.user.name}`;
+      this.profilePicSrc = this.user.picture.data.url;
+    }
   }
 }
 
