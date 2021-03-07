ec86c0fe7e07098e6ef91f1a189dd32aa6c11da3
djd6797 <77515007+djd6797@users.noreply.github.com>
2021-03-06 20:09:50 -0800

    Load categories page data from server
    included an exDatabase.json file to describe how the data is organized on the server (remove from production)

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -6,6 +6,7 @@ import { isPlatformBrowser } from '@angular/common';
 
 import { FacebookProviderService } from '../facebook-provider.service';
 import { profile } from 'console';
+import * as firebase from 'firebase';
 
 
 @Component({
@@ -21,6 +22,9 @@ export class CategoriesPage implements AfterViewInit {
   user = null;
   profilePic: HTMLImageElement;
   clickHandled: Boolean = false;
+  auctionsRef : firebase.database.Reference = null;
+  liveAucs = {};
+  pastAucs = {};
 
   constructor(
     public router: Router,
@@ -93,10 +97,68 @@ export class CategoriesPage implements AfterViewInit {
         userGreeting.innerHTML = "Hi, no one is online. This screen shouldn't be viewable.";
         this.profilePic.src = "https://habib.al-mawali.com/wp-content/uploads/IMG_4838-1-768x768.jpg";
         this.router.navigate(['walkthrough']);
+        return false;
       }
       else {
         userGreeting.innerHTML = `Hey ${ret.name},`;
         this.profilePic.src = ret.picture.data.url;
+        return true;
+      }
+    }).then((success) => {
+      if (!success) {return;}
+      this.auctionsRef = firebase.database().ref('auctions');
+      this.auctionsRef.on('child_added', (data) => {
+        this.addAuction(data);
+      });
+      this.auctionsRef.on('child_changed', (data) => {
+        this.setAuction(data);
+      });
+      this.auctionsRef.on('child_removed', (data) => {
+        this.deleteAuction(data);
+      });
+    });
+  }
+  
+  addAuction(data: firebase.database.DataSnapshot): void {
+    if (data.val().aucEnd < 1922) {
+      this.pastAucs[data.key] = data.val();
+    }
+    else {
+      this.liveAucs[data.key] = data.val();
+    }
+  }
+
+  setAuction(data: firebase.database.DataSnapshot): void{
+    if (data.val().aucEnd < 1922) {
+      this.pastAucs[data.key] = data.val();
+    }
+    else {
+      this.liveAucs[data.key] = data.val();
+    }
+  }
+
+  deleteAuction(data: firebase.database.DataSnapshot): void{
+    if (data.val().aucEnd < 1922) {
+      this.pastAucs[data.key] = null;
+    }
+    else {
+      this.liveAucs[data.key] = null;
+    }
+  }
+
+  writeData(): void {
+    console.log('writing data');
+    let res = this.facebookProvider.getUser();
+    res.then((ret) => {
+      if (ret == null) {
+        firebase.database().ref('lastUser').set({
+          name: 'anonymous'
+        });
+      }
+      else {
+        firebase.database().ref('lastUser').set({
+          name: ret.name
+        });
       }
     });
   }
